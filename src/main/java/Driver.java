dc553cb86e88371ce8ad2c4f8bc675640201a31a
madF <58264400+fakih06@users.noreply.github.com>
2022-06-09 22:48:18 +0800

    commit

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -16,6 +16,17 @@ public class Driver {
 		
 		System.out.println("Current count:" + counter.getCount());
 		
+		counter.increaseBy(2);
+		
+		System.out.println("Current count:" + counter.getCount());
+
+		counter.decreaseBy(5);
+		
+		System.out.println("Current count:" + counter.getCount());
+
+		counter.multiplyBy(6);
+		
+		System.out.println("Current count:" + counter.getCount());
 	}
 
 }
